bfd547c53c1d649bc1e1c511f35d9dce651ef7cd
Tarun <gtarun14111@gmail.com>
2020-05-13 17:49:09 +0530

    Updates

---
+++ precache-manifest.1719c4a7612282acd8dfe7890d10c959.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7eef790aec4c89d3e211dc4d9da480",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fecd30cd3deb633739ed",
+    "url": "/portfolio/static/css/main.59ec0d24.chunk.css"
+  },
+  {
+    "revision": "ed61c96e6c65547e18a7",
+    "url": "/portfolio/static/js/2.c09a468d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c09a468d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecd30cd3deb633739ed",
+    "url": "/portfolio/static/js/main.28a82e67.chunk.js"
+  },
+  {
+    "revision": "aa34148ee296db0ebcc8",
+    "url": "/portfolio/static/js/runtime-main.7645ef8c.js"
+  },
+  {
+    "revision": "f24577e88a327bd1da2d41526e89dfb3",
+    "url": "/portfolio/static/media/College.f24577e8.jpg"
+  },
+  {
+    "revision": "1b2269604a996bcb90ea45abb49a55ac",
+    "url": "/portfolio/static/media/Kaneki.1b226960.jpg"
+  },
+  {
+    "revision": "52ef38728623c9070855af3a25e01798",
+    "url": "/portfolio/static/media/Tarun.52ef3872.jpg"
+  },
+  {
+    "revision": "19dd23c64ea2c8c0f15e471fd72cd738",
+    "url": "/portfolio/static/media/csssc.19dd23c6.png"
+  },
+  {
+    "revision": "aa01054338f2c5a35a9acf439cfb1fa9",
+    "url": "/portfolio/static/media/expresssc.aa010543.png"
+  },
+  {
+    "revision": "d5e7bb96c5aaa1723197bdd4386bacdd",
+    "url": "/portfolio/static/media/jssc.d5e7bb96.png"
+  },
+  {
+    "revision": "ee11dc5aa5639239f1c9374bc9cd74d5",
+    "url": "/portfolio/static/media/nodesc.ee11dc5a.png"
+  },
+  {
+    "revision": "f1e4726a2342b8a237ccb22f912c8551",
+    "url": "/portfolio/static/media/postgressc.f1e4726a.png"
+  },
+  {
+    "revision": "57a534acac8bfcbdc6e2497af7261dfc",
+    "url": "/portfolio/static/media/reactsc.57a534ac.png"
+  }
+]);
